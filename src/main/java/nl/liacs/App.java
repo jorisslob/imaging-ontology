842ea67b08f4d978dc77b6b371f1affff281086a
Joris Slob <jorisslob@gmail.com>
2014-08-27 20:31:10 +0200

    Added a travis configuration file and generated pom.xml for the project.

---
+++ src/main/java/nl/liacs/App.java
@@ -0,0 +1,13 @@
+package nl.liacs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
