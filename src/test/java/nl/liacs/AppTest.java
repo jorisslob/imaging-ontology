57be33c4d6a520bb0daeed6431818314b6592027
Joris Slob <jslob@liacs.nl>
2014-08-28 14:57:50 +0200

    Closes #5. Added log4j as testing resource and added test for PROV-O testing.

--- src/test/java/nl/liacs/AppTest.java
+++ src/test/java/nl/liacs/AppTest.java
@@ -1,5 +1,7 @@
 package nl.liacs;
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import java.io.File;
 import java.net.URL;
 import junit.framework.Test;
@@ -9,34 +11,45 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
+    private final URL onto_url;
+    private final String FILENAME = "imaging-ontology.owl";
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
+    public AppTest( String testName ) {
         super( testName );
+        Thread my_thread = Thread.currentThread();
+        ClassLoader my_classloader = my_thread.getContextClassLoader();
+      	onto_url = my_classloader.getResource("imaging-ontology.owl");
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite( AppTest.class );
     }
 
     /**
      * Check that the ontology is named correctly
      */
-    public void testOntologyName()
-    {
-	URL url = Thread.currentThread().getContextClassLoader().getResource("imaging-ontology.owl");
-	File file = new File(url.getPath());
-        assertEquals(file.getName(),"imaging-ontology.owl");
+    public void testOntologyName() {
+        File onto_file = new File(onto_url.getPath());
+        String error_msg = "Ontology filename is not correct";
+        assertEquals(error_msg, onto_file.getName(), FILENAME);
+    }
+    
+    /**
+     * Check if PROV is imported
+     */
+    public void testImportPROV()  {
+        OntModel m = ModelFactory.createOntologyModel();
+        m.read(onto_url.toString());
+        String error_msg = "Ontology doesn't import PROV-O 20130430";
+        String prov_uri = "http://www.w3.org/ns/prov-o-20130430";
+        assertTrue(error_msg, m.hasLoadedImport(prov_uri));
     }
 }
