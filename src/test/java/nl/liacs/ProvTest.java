7d4e204bd4c825036847fd388d88b44b678eea85
Joris Slob <jslob@liacs.nl>
2014-09-04 10:18:39 +0200

    Added HermiT reasoner because Jena did a poor job detecting problems.

--- src/test/java/nl/liacs/ProvTest.java
+++ src/test/java/nl/liacs/ProvTest.java
@@ -1,29 +1,27 @@
 package nl.liacs;
 
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.rdf.model.InfModel;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.reasoner.Reasoner;
-import com.hp.hpl.jena.reasoner.ReasonerRegistry;
-import com.hp.hpl.jena.reasoner.ValidityReport;
-import com.hp.hpl.jena.util.FileManager;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 /**
  * Unit test for simple App.
  */
 public class ProvTest extends TestCase {
-    private final URL onto_url;
     private final URL data_url;
-    private final Model schema;
-    private final Model data;
-    private final OntModel model;
-    private final InfModel inf;
-    private final String PROV_URI = "http://www.w3.org/ns/prov-o-20130430";    
+    private final String PROV_URI = "http://www.w3.org/ns/prov-o#";    
+    private OWLOntology prov_data_model;
     
     /**
      * Create the test case
@@ -34,17 +32,15 @@ public class ProvTest extends TestCase {
         super( testName );
         Thread my_thread = Thread.currentThread();
         ClassLoader my_classloader = my_thread.getContextClassLoader();
-      	onto_url = my_classloader.getResource("imaging-ontology.owl");
         data_url = my_classloader.getResource("prov-example.rdf");
         
-        schema = FileManager.get().loadModel(onto_url.toString());
-        data = FileManager.get().loadModel(data_url.toString());
-        Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-        reasoner = reasoner.bindSchema(schema);
-        inf = ModelFactory.createInfModel(reasoner, data);
-        
-        model = ModelFactory.createOntologyModel();
-        model.read(onto_url.toString());
+        prov_data_model = null;
+        try {            
+            OWLOntologyManager m = OWLManager.createOWLOntologyManager();
+            prov_data_model = m.loadOntologyFromOntologyDocument(IRI.create(data_url));
+        } catch (URISyntaxException | OWLOntologyCreationException ex) {
+            Logger.getLogger(ProvTest.class.getName()).log(Level.SEVERE, null, ex);
+        } 
     }
 
     /**
@@ -58,16 +54,22 @@ public class ProvTest extends TestCase {
      * Check if PROV is imported
      */
     public void testImportPROV()  {
-        String error_msg = "Ontology doesn't import PROV-O 20130430";
-        assertTrue(error_msg, model.hasLoadedImport(PROV_URI));
+        Set<OWLOntology> imports = prov_data_model.getImportsClosure();
+        boolean foundProv = false;
+        for(OWLOntology i:imports) {
+            if(i.getOntologyID().getOntologyIRI().toString().equals(PROV_URI)) {
+                foundProv = true;
+            }
+        }
+        String error_msg = "Example rdf file does not import PROV";
+        assertTrue(error_msg, foundProv);
     }
     
     /**
      * Check if the ontology is valid and clean (here called consistent)
      */
     public void testConsistency() {
-        ValidityReport validation = inf.validate();
-        assertTrue(validation.isValid());
-        assertTrue(validation.isClean());
+        Reasoner hermit = new Reasoner(prov_data_model);
+        assertTrue(hermit.isConsistent());
     }
 }
